f65cd3070ad7d92ad4da40add142a612763d86d3
Sumitra <sumitrac2008@gmail.com>
2021-03-29 03:00:45 -0700

    Calculator in Python

---
+++ PSE_1/main.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter first number: "))
+op = input("Enter a operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2) 
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "%":
+    print(num1 % num2)
+else: 
+    print("Please enter a vaild operator: ")
+
+
+
+
